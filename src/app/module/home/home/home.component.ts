de9ec424258f7998077ebe095d6bdc7ac255289d
Andres <conocimineto.02@gmail.com>
2024-09-18 11:12:48 -0500

    actualizacion

--- src/app/module/home/home/home.component.ts
+++ src/app/module/home/home/home.component.ts
@@ -10,7 +10,7 @@ import { ThemeModeService } from 'src/app/core/services/modoOscuro/theme-mode.se
 })
 export class HomeComponent implements OnInit{
   mode!: string;
-  computer = '../../../../assets/images/computer.png';
+  computer = '../../../../assets/images/1.png';
   Colash1 = '../../../../assets/images/Colash1.png';
   Colash2 = '../../../../assets/images/Colash2.png';
   targeta = '../../../../assets/images/targeta.png';
@@ -20,8 +20,9 @@ export class HomeComponent implements OnInit{
   Efecty = '../../../../assets/images/Efecty.png';
   PSE = '../../../../assets/images/PSE.png';
   Nequi = '../../../../assets/images/Nequi.png';
-  cel = '../../../../assets/images/cel.png';
+  cel = '../../../../assets/images/2.png';
   Footer = '../../../../assets/images/Footer.png';
+lo: any;
   constructor(private themeModeService: ThemeModeService) { }
 
   ngOnInit(): void {
