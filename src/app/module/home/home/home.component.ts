9638356e22708189d3d25d07dd1ea5abf2561d74
Felipe0206 <conocimiento.02@gmail.com>
2024-10-21 12:56:55 -0500

    Correccion de  errores y eliminacion de elementos

--- src/app/module/home/home/home.component.ts
+++ src/app/module/home/home/home.component.ts
@@ -11,8 +11,8 @@ import { ThemeModeService } from 'src/app/core/services/modoOscuro/theme-mode.se
 export class HomeComponent implements OnInit{
   mode!: string;
   computer = '../../../../assets/images/1.png';
-  Colash1 = '../../../../assets/images/Colash1.png';
-  Colash2 = '../../../../assets/images/Colash2.png';
+  Colash1 = '../../../../assets/images/Colash-1.png';
+  Colash2 = '../../../../assets/images/Colash-2.png';
   targeta = '../../../../assets/images/targeta-1.png';
   Beneficio = '../../../../assets/images/Mesa de trabajo 34 (1).png';
   wallet = '../../../../assets/images/wallet.png';
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit{
   Efecty = '../../../../assets/images/Efecty.png';
   PSE = '../../../../assets/images/PSE.png';
   Nequi = '../../../../assets/images/Nequi.png';
-  cel = '../../../../assets/images/2.png';
+  cel = '../../../../assets/images/cel-1.png';
   Footer = '../../../../assets/images/Footer-1.png';
 lo: any;
   constructor(private themeModeService: ThemeModeService) { }
